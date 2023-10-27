3cf7f3855f4e57b531fbd9c255b496a36e4dfbe6
Seoyoon <wideskyinme@gmail.com>
2023-10-27 19:13:20 +0900

    init

---
+++ src/main/java/com/example/springassignmentlv1/SpringAssignmentLv1Application.java
@@ -0,0 +1,13 @@
+package com.example.springassignmentlv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssignmentLv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAssignmentLv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springassignmentlv1/SpringAssignmentLv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springassignmentlv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAssignmentLv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
