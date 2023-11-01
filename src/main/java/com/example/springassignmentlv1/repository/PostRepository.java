9b6a69a3e5c81067445b6e1b7e268f48671e2530
Seoyoon <wideskyinme@gmail.com>
2023-11-01 19:45:50 +0900

    Refactor: @Repository 어노테이션 추가

--- src/main/java/com/example/springassignmentlv1/repository/PostRepository.java
+++ src/main/java/com/example/springassignmentlv1/repository/PostRepository.java
@@ -3,9 +3,11 @@ package com.example.springassignmentlv1.repository;
 import com.example.springassignmentlv1.dto.PostResponseDto;
 import com.example.springassignmentlv1.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByOrderByModifiedAtDesc();
 }
